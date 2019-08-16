62a22993d7c6d30f52229c344fd5f8a9355e7765
EQwon <bluetwintail0906@gmail.com>
2019-08-16 20:10:15 +0900

    Add Dummy Image

---
+++ Assets/Scripts/StationInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class StationInfo
+{
+    public string name;
+    public bool isReversible;
+    public bool isToilet;
+}
---
+++ Assets/Scripts/StationShower.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationShower : MonoBehaviour
+{
+    public StationInfo info;
+
+    public GameObject ActionBubble;
+    public GameObject InfoBubble;
+
+    private void Start()
+    {
+        ActionBubble.SetActive(false);
+        InfoBubble.SetActive(false);
+    }
+
+    private void InfoInitialize()
+    {
+
+    }
+
+    public void GoToToilet()
+    {
+        Debug.Log(info.name + "역에서 화장실에 갑니다.");
+    }
+
+    public void Begging()
+    {
+        Debug.Log(info.name + "역에서 Action 2");
+    }
+
+    public void Action3()
+    {
+        Debug.Log(info.name + "역에서 Action 3");
+    }
+
+    public void Action4()
+    {
+        Debug.Log(info.name + "역에서 Action 4");
+    }
+}
