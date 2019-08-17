ed911ebfd9286a40d7010ac1b167a1583d52587a
bbtarzan12 <bbtarzan12@gmail.com>
2019-08-18 00:44:31 +0900

    역에 있는 승객에 따라 역에 표시

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Player : MonoBehaviour, IPassenger
 {
     [SerializeField] Station startStation;
-
+    public Color playerColor;
     #region Status
     
     //돈
--- Assets/Scripts/Station.cs
+++ Assets/Scripts/Station.cs
@@ -19,22 +19,27 @@ public class Station : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     public List<IPassenger> GetBoardingPassengers => passengers.FindAll(p => p.WantToGetIn);
     public bool IsPlayerInStation => passengers.Exists(p => p is Player);
+
+    Image circle;
+
+    void Awake()
+    {
+        circle = transform.GetChild(2).GetComponent<Image>();
+    }
     
     // 열차를 타기 위해 Station -> Train으로 이동
     public void Out(IPassenger passenger)
     {
         Debug.Log($"{passenger}이 {name} 역에서 나감");
         passengers.Remove(passenger);
+        
+        Refresh();
     }
 
     // 열차에서 사람이 내린 후 Station에 도착
     public void Enter(IPassenger passenger)
     {
         Debug.Log($"{passenger}이 {name} 역에 도착");
-        
-        passenger.Transform.SetParent(transform.GetChild(passenger.IsOpposite ? 0 : 1), false);
-        passenger.Transform.localPosition = Vector3.zero;
-        passenger.Transform.localRotation = Quaternion.identity;
 
         passengers.Add(passenger);
 
@@ -42,20 +47,47 @@ public class Station : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         {
             GameManager.instance.Meeting();
         }
+        
+        Refresh();
     }
 
     public void Remove(IPassenger passenger)
     {
-        
+        passengers.Remove(passenger);
     }
 
     public void Refresh()
     {
+        bool player = false;
+        bool agent = false;
+        Color color = Color.black;
         foreach (IPassenger passenger in passengers)
         {
+            if (passenger is Player p)
+            {
+                color += p.playerColor;
+                player = true;
+            }
+            else
+            {
+                agent = true;
+            }
             passenger.Transform.SetParent(transform.GetChild(passenger.IsOpposite ? 0 : 1), false);
             passenger.Transform.localPosition = Vector3.zero;
             passenger.Transform.localRotation = Quaternion.identity;
+        }
+
+        if (agent)
+        {
+            circle.color = Color.black;
+        }
+        else if(player)
+        {
+            circle.color = color;
+        }
+        else
+        {
+            circle.color = new Color(0,0,0,0);
         }
     }
 
